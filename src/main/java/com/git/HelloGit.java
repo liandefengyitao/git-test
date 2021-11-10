d076d6e4506a7600e78baaaeb65b219542780bd8
liandefengyitao <liandefengyitao@foxmail.com>
2021-11-10 15:17:23 +0800

    master conflicted commit by idea.

--- src/main/java/com/git/HelloGit.java
+++ src/main/java/com/git/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!!!");
         System.out.println("Continue to commit");
+        System.out.println("前一条会发生冲突！！！");
     }
 }
