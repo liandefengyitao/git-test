a525a1cbdfc700b6a017b0bf37c6d3967c16f178
liandefengyitao <liandefengyitao@foxmail.com>
2021-11-10 15:05:21 +0800

    second commit by idea.

--- src/main/java/com/git/HelloGit.java
+++ src/main/java/com/git/HelloGit.java
@@ -3,5 +3,7 @@ package com.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!!!");
+        System.out.println("Only hot-fix can see!");
+        System.out.println("Continue to commit");
     }
 }
